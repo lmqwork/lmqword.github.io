f403683e288514e77f16f63481f429746ca65753
quanglm8 <quanglm8@fsoft.com.vn>
2023-02-08 10:35:20 +0700

    + add gpt

--- android/app/src/main/kotlin/com/example/auto_gpt_web/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.auto_gpt_web
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/src/presentation/helper/helper.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:responsive_framework/responsive_wrapper.dart';
-
-class Helper {
-  static bool isDesktop(BuildContext context) {
-    final isDesktop = ResponsiveWrapper.of(context).isLargerThan(TABLET);
-    return isDesktop;
-  }
-}
